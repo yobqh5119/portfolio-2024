2ecb0a8fee62e645f9b3f8d2876b49f35af8d4a1
yoobin <yoobin.hong@ingka.ikea.com>
2024-06-30 17:23:54 -0400

    working on the bento grid section

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,17 @@
 import Hero from "@/components/Hero";
 import Image from "next/image";
+import { FaHome } from "react-icons/fa";
+import { FloatingNav } from "@/components/ui/FloatingNav";
+import Grid from "@/components/Grid";
+import { navItems } from "@/data";
 
 export default function Home() {
   return (
    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
     <div className="max-w-7xl w-full">
+      <FloatingNav navItems={navItems}/>
       <Hero />
-      <h1>Hello Portfolio</h1>
+      <Grid />
     </div>
    </main>
   );
--- components/Grid.tsx
+++ components/Grid.tsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import { BentoGrid, BentoGridItem } from './ui/BentoGrid'
+import { gridItems } from '@/data'
 
 const Grid = () => {
   return (
-    <div>Grid</div>
+    <section id='about'>
+      <BentoGrid>
+        {gridItems.map(({id, title, description, img, className, imgClassName, titleClassName, spareImg}) => (
+          <BentoGridItem 
+            key={id}
+            id={id}
+            title={title}
+            description={description}
+            img={img}
+            imgClassName={imgClassName}
+            titleClassName={titleClassName}
+            spareImg={spareImg}
+            className={className}
+          />
+        ))}
+      </BentoGrid>
+    </section>
   )
 }
 
